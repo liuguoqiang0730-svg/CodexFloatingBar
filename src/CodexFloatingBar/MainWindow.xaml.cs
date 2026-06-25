6498f69a22e1ccbc388ca21aa5868d2817b3b40e
liyuejiong <yuejiong.li@ehang.com>
2026-06-25 09:23:00 +0800

    Widen floating status bar

--- src/CodexFloatingBar/MainWindow.xaml.cs
+++ src/CodexFloatingBar/MainWindow.xaml.cs
@@ -137,7 +137,7 @@ public partial class MainWindow : Window
 
     private void ApplyAppearance()
     {
-        Width = 560 * _appearanceSettings.Scale;
+        Width = 720 * _appearanceSettings.Scale;
         Height = 92 * _appearanceSettings.Scale;
         MinWidth = Width;
         MinHeight = Height;
